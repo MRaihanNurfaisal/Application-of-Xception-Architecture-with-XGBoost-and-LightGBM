a1277657f0f3ae660263f0b26bce2d0c165939b9
Muhammad Raihan Nurfaisal <163430287+MRaihanNurfaisal@users.noreply.github.com>
2025-07-23 21:01:09 +0700

    Add files via upload

---
+++ appp.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Flask! aldskjjjjjjjjjjjjjjjjjjjjjjjjjjjjskaljklaasdjlkasdjlkasdjlkasdjlkdfowheroiugroqcnp9wtyq09wevybtoqwuh qiwuthvb9034ui19p8209158375i quhfjljDBvaehrou"
+
+if __name__ == '__main__':
+    app.run(debug=True)
